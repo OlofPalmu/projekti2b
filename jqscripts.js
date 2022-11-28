0abbb53971dacb5161a9e956760ccb3951c62b0f
OlofPalmu <ville.palmu@student.laurea.fi>
2022-11-28 20:43:21 +0200

    Aloitus ja pään tuhoaminen
    Hakattu varmaan 4h. Nyt alkaa riittää yhelle illalle, ei aivot enään toimi.

--- jqscripts.js
+++ jqscripts.js
@@ -1,1 +1,34 @@
+$(function () {
 
+  var $tasksList = $("#tasksList");
+  var $taskInput = $("#taskInput");
+
+  $("#taskAdd").on("click", function () {
+    if (!$taskInput.val()) {
+      $taskInput.css("border", "solid 3px red");
+      alert("Kirjoita tekstikenttään jotain!");
+      return false;
+    } else {
+      $tasksList.append(
+        "<li>" +
+          $taskInput.val() +
+          "<button class='delete'>&#10006</button></li>"
+      );
+      $taskInput.css("border", "none");
+    }
+
+    
+    $taskInput.val("");
+
+    $(".delete").on("click", function () {
+      var $parent = $(this).parent();
+
+      $tasksList.css("none");
+
+      $parent.css("animation", "fadeOut .3s linear");
+      setTimeout(function () {
+        $parent.remove();
+      }, 295);
+    });
+    });
+});
